2b6d5df1eb07d13b13199a21b2743986353d34f1
hexie2108 <40892639+hexie2108@users.noreply.github.com>
2018-11-25 17:05:27 +0100

    fixed bug of find()

--- mdb/classes/Active_Users.js
+++ mdb/classes/Active_Users.js
@@ -1,56 +1,86 @@
 function makeid(len) {
-    var text = ""; var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (var i = 0; i < len; i++){
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-  }
-class Active_User{
-    /*
-    token(string) PRIMARY KEY
-    user(User) UNIQUE
-    */
-    constructor(user){
-        this.token = makeid(8);
-        this.user = user;
-    }
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        for (var i = 0; i < len; i++)
+        {
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        return text;
 }
-class Active_Users extends Array{
-    //ADD METHOD
-    add(user){
-        console.log("got user -> " + user);
-        var x = null;
-        if(this.length === 0){
-            x = new Active_User(user);
-        }else{
-            if(this.find(obj => obj.user.email === user.email) === undefined){
-                x = new Active_User(user);
-            }
+class Active_User {
+        /*
+         token(string) PRIMARY KEY
+         user(User) UNIQUE
+         */
+        constructor(user) {
+                this.token = makeid(8);
+                this.user = user;
         }
-        if(x !== null){
-            this.push(x);
+}
+class Active_Users extends Array {
+        //ADD METHOD
+        add(user) {
+                console.log("got user -> " + user);
+                var x = null;
+                if (this.length === 0)
+                {
+                        x = new Active_User(user);
+                }
+                else
+                {
+                        if (this.find(obj => obj.user.email === user.email) === undefined)
+                        {
+                                x = new Active_User(user);
+                        }
+                }
+                if (x !== null)
+                {
+                        this.push(x);
+                }
+                console.log("Active users length : " + this.length);
         }
-        console.log("Active users length : " + this.length);
-    }
-    //GET METHODS
-    getTokenByUser(user){
-        return this[this.indexOf(this.find(obj => obj.user.email === user.email))].token;
-    }
-    getUserByToken(token){
-        return this[this.indexOf(this.find(obj => obj.token === token))].user;
-    }
-    //DELETE METHODS
-    deleteByUser(user){
-        var index = this.indexOf(this.find(obj => obj.user.email === user.email));
-        if(index>=0){
-            this.splice(index,1);
+        //GET METHODS
+        getTokenByUser(user) {
+
+                let userFound = this.find(obj => obj.user.email === user.email);
+                if (userFound !== undefined)
+                {
+                        return this[this.indexOf(userFound)].token;
+                }
+                else
+                {
+                        return null;
+                }
+
         }
-    }
-    deleteById(id){
-        var index = this.indexOf(this.find(obj => obj.id === id));
-        if(index>=0){
-            this.splice(index,1);
+        getUserByToken(token) {
+
+                let tokenFound = this.find(obj => obj.token === token);
+                if (tokenFound !== undefined)
+                {
+                        return this[this.indexOf(tokenFound)].user;
+                }
+                else
+                {
+                        return null;
+                }
+
+
         }
-    }
+        //DELETE METHODS
+        deleteByUser(user) {
+                var index = this.indexOf(this.find(obj => obj.user.email === user.email));
+                if (index >= 0)
+                {
+                        this.splice(index, 1);
+                }
+        }
+        deleteById(id) {
+                var index = this.indexOf(this.find(obj => obj.id === id));
+                if (index >= 0)
+                {
+                        this.splice(index, 1);
+                }
+        }
 }
 module.exports = Active_Users;
