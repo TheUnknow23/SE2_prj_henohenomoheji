081f662e5efecb87077d18d1b2822d32712fd6b6
TheUnknow23 <32764086+TheUnknow23@users.noreply.github.com>
2018-11-26 11:29:40 +0100

    Add groups branch

--- routes/groups.js
+++ routes/groups.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const mdb = require ('./../mdb/mdb.js');
 
+/*##### FUNCTION SECTION #####*/
+
+// GET on /groups
+function routerGetGroups(token){
+        let requester = mdb.active_users.getUserByToken(token);
+        if (requester !== null && requester.type === 'teacher'){
+                let groups = mdb.groups.getAll();
+        }
+}
+
 router.get('/', function(req, res) {
         res.send('groups resources');
 });
