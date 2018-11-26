0bce40a201c7e3cc10191e457bb99cade760add3
g1nus <giuliano.andronic@studenti.unitn.it>
2018-11-26 17:04:50 +0100

    add tests for /exam_submissions POST

--- test/exam_submission.test.js
+++ test/exam_submission.test.js
@@ -22,7 +22,7 @@ test('call display_exam_submission with correct token and id(as submitter)', () 
     expect(es.display_exam_submission(mdb.active_users[1].token, 0)).toBe(mdb.exam_submissions[0]);
 });
 test('call display_exam_submission with correct token and id(as reviewer)', () => {
-    expect(es.display_exam_submission(mdb.active_users[2].token, 0)).toBe(mdb.exam_submissions[0]);
+    expect(es.display_exam_submission(mdb.active_users[3].token, 0)).toBe(mdb.exam_submissions[0]);
 });
 
 //display_exam_submission_list() tests
@@ -33,8 +33,22 @@ test('call display_exam_submission_list with incorrect token', () => {
     expect(es.display_exam_submission_list("dsdsdsdssdsd", "toreview")).toBe("error null");
 });
 test('call display_exam_submission_list with incorrect query param', () => {
-    expect(es.display_exam_submission_list(mdb.active_users[0].token, "torevifew")).toBe("error null");
+    expect(es.display_exam_submission_list(mdb.active_users[3].token, "torevifew")).toBe("error null");
 });
 test('call display_exam_submission_list with correct values', () => {
-    expect(es.display_exam_submission_list(mdb.active_users[1].token, "owned")).toEqual(mdb.exam_submissions);
+    expect(es.display_exam_submission_list(mdb.active_users[0].token, "owned")).toEqual(mdb.exam_submissions);
+});
+
+//insert_exam_submission() tests
+test('call insert_exam_submission with no parameters', () => {
+    expect(es.insert_exam_submission()).toBe("error null");
+});
+test('call insert_exam_submission with incorrect token', () => {
+    expect(es.insert_exam_submission("dsdsdsdssdsd", {"ref_exam": 0,"answers":["bob","booby"], "status": "on hold"})).toBe("error null");
+});
+test('call insert_exam_submission with incorrect payload', () => {
+    expect(es.insert_exam_submission(mdb.active_users[2].token, {"ref_exam": 0,"answers":["bob","booby"], "status": 2})).toBe("error null");
+});
+test('call insert_exam_submission with correct values', () => {
+    expect(es.insert_exam_submission(mdb.active_users[2].token,  {"ref_exam": 0,"answers":["bob","booby"], "status": "on hold"})).toEqual(1);
 });
