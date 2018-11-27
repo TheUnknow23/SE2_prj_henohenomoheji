243d052583401064ef1712052414dc4c44842e4b
luca-morgese97 <luca.morgese@studenti.unitn.it>
2018-11-27 11:38:05 +0100

    add first significative test for /users

--- test/users.test.js
+++ test/users.test.js
@@ -1,3 +1,13 @@
-test('dummy test', () => {
-    expect(3).toBe(3);
+
+const routerGetUsers = require('../routes/users.js').routerGetUsers;
+const routerGetUserById = require('../routes/users.js').routerGetUserById;
+const mdb = require('../mdb/mdb.js');
+
+test('GET /users with requester != requested', () => {
+    let user = routerGetUserById(mdb.active_users.getTokenByUserId(0), 1);
+    expect(user.id).toBeDefined();
+    expect(user.name).toBeDefined();
+    expect(user.surname).toBeDefined();
+    expect(user.email).toBeDefined();
+    expect(user.password).toBeUndefined();
 });
