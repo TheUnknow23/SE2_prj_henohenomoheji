d57dfc455f1d587e1b08776020aada5378936d51
g1nus <giuliano.andronic@studenti.unitn.it>
2018-11-27 20:49:38 +0100

    exam filter fix

--- mdb/classes/Exam_Submission.js
+++ mdb/classes/Exam_Submission.js
@@ -35,7 +35,7 @@ class Exam_Submissions extends Array{
     }
     //FILTER METHODS
     filterByExam(exam){
-        return this.find(obj => obj.ref_exam.id === exam.id);
+        return this.filter(obj => obj.ref_exam.id === exam.id);
     }
     filterBySubmitter(submitter){
         return this.filter(obj => obj.submitter.email === submitter.email);
