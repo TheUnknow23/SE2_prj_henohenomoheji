262466d0628461ac3030d2c61097fa6979ef642d
g1nus <giuliano.andronic@studenti.unitn.it>
2018-12-02 16:16:17 +0100

    extra functions for peer reviews class

--- mdb/classes/Exam_Peer_Review.js
+++ mdb/classes/Exam_Peer_Review.js
@@ -69,6 +69,11 @@ class Exam_Peer_Reviews extends Array{
     findById(id){
         return this.find(obj => obj.id === id);
     }
+    getIndexByUserAndSubmission(user, ref_sub){
+        var index = this.indexOf(this.find(obj => (obj.submission.id === ref_sub.id && obj.reviewer.id === user.id)));
+        //console.log("found the following review ->");console.log(rev);
+        return index;
+    }
     //DELETE METHODS
     deleteById(id){
         var index = this.indexOf(this.find(obj => obj.id === id));
@@ -76,6 +81,7 @@ class Exam_Peer_Reviews extends Array{
             this.splice(index,1);
         }
     }
+    //CHECK METHODS
     hasReview(exam, user){//checks is there's already an review for the given exam and user
         var rev = this.find(obj => (obj.submission.ref_exam === exam && obj.reviewer === user));
         //console.log("found the following review ->");console.log(rev);
