2191f94702ba5aa82f2fa3bb27713cdffad069c7
g1nus <giuliano.andronic@studenti.unitn.it>
2018-12-06 15:10:34 +0100

    add logout functionality

--- routes/logout.js
+++ routes/logout.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const mdb = require ('./../mdb/mdb.js');
+const logic = require('./logic/logout_logic');
 
 router.get('/', function(req, res) {
+        logic.logoutFunction(req.query.token);
         res.send('logout resources');
 });
 
